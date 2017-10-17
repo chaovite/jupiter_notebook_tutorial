a87a37bf4394c26a6d25b38f7669f86c746f77ed
chaovite <chaovite@gmail.com>
2017-10-17 14:06:31 -0700

    add a demo module to the repo

---
+++ mymodule.py
@@ -0,0 +1,13 @@
+"""
+This is a simple module where you can write your subroutines or classes
+"""
+
+def reverseList(a):
+    """ reverse a python list
+    """
+    return a[::-1]
+
+def addTwoNums(a, b = 0):
+    """ add two numbers and set default of b to be zero
+    """
+    return a + b
